09e92ddceebfcb8429d185864524ab877dd195cb
Aum Dhokia <aumdhokia64@gmail.com>
2026-05-25 17:20:34 +0530

    Add files via upload
    Total And Percentage Calculator In Python !!
    { For Only 40 Marks Test/Exam Only }
    
    For 100 Marks , It Will Be Uploaded Soon !!

---
+++ totalandpercentagemarks.py
@@ -0,0 +1,16 @@
+#For 40 Marks Test/Exam Only
+
+num1 = int(input("Enter Your 1st Subject Marks"))
+num2 = int(input("Enter Your 2nd Subject Marks"))
+num3 = int(input("Enter Your 3rd Subject Marks"))
+num4 = int(input("Enter Your 4th Subject Marks"))
+num5 = int(input("Enter Your 5th Subject Marks"))
+
+total = num1 + num2 + num3 + num4 + num5
+print("Total Marks:", total)
+
+avg_marks = total/5
+print("Your Average Marks:", avg_marks)
+
+percentage = total/200 * 100
+print("Your Percentage:", percentage)
